5ded6f5a992984c6eaa477f66742d25ad7f8428c
mohamed sami <samy.ashoor@gmail.com>
2017-10-26 21:09:12 +0200

    Update Weather.vue

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -4,7 +4,7 @@
       <div class="row">
         <div class="col-lg-5">
           <div class="form-group">
-            <label for="exampleSelect1">select country</label>
+            <label for="exampleSelect1">select city</label>
             <select class="form-control" v-model="selected" @change="fetchWeather">
                 <option value="Egypt" selected>Egypt</option>
                 <option value="russian">russian</option>
@@ -18,7 +18,7 @@
 
         <div class="col-lg-5">
           <div class="form-group">
-            <label for="exampleSelect1">select city</label>
+            <label for="exampleSelect1">select country</label>
             <select class="form-control" v-model="selected" @change="fetchWeather">
               <option value="city" selected>city</option>
               <option value="cairo">cairo</option>
@@ -40,7 +40,7 @@
             <h3>Weather in {{selected}}</h3>
         </div>
         <div class="list-group-item d-flex justify-content-between" v-bind:class="[booleanValue ? 'col-lg-6' : 'col-lg-12']" v-for="(i, index) in city.list" v-if="index < 4">
-          {{i.dt_txt}} <span class="d-flex align-items-center"><h5>{{i.main.humidity}}</h5> <h1>°C</h1></span>
+          {{i.dt_txt}} <span class="d-flex align-items-center"><h5>{{i.main.temp}}</h5> <h1>°C</h1></span>
         </div>
       </div>
     </div>
@@ -60,7 +60,7 @@
         },
         methods: {
           fetchWeather(){
-            this.$http.get('http://api.openweathermap.org/data/2.5/forecast?q='+this.selected+'&mode=json&appid=5121f13fd90c7f33f764109bcfc0c9fd').then(function(res){
+            this.$http.get('http://api.openweathermap.org/data/2.5/forecast?q='+this.selected+'&mode=json&units=metric&appid=5121f13fd90c7f33f764109bcfc0c9fd').then(function(res){
               this.city = res.body;
             });
           },
